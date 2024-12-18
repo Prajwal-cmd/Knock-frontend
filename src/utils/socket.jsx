c678281afa8eb8d4ccaeeda7d2ae49cd1e95c1f0
Prajwal-cmd <136264835+Prajwal-cmd@users.noreply.github.com>
2024-12-18 20:30:17 +0530

    Update socket.jsx

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -15,13 +15,7 @@ const getSocket =() =>useContext(SocketContext)
 
 const SocketProvider = ({children})=>{
     const socket = useMemo(()=>io(server,{withCredentials:true,transports: ["polling","websocket"]}),[])
-    socket.on("connect", () => {
-  const transport = socket.io.engine.transport.name; // in most cases, "polling"
-
-  socket.io.engine.on("upgrade", () => {
-    const upgradedTransport = socket.io.engine.transport.name; // in most cases, "websocket"
-  });
-}); //
+    
 
     return (
         <SocketContext.Provider value={socket} >
